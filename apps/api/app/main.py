a0ed9c1a434833e2b064a02fa3eefbb2092177a4
Suhaas Badada <suhaasbadada@gmail.com>
2026-04-27 14:47:38 -0400

    cors update

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -8,7 +8,10 @@ app = FastAPI(title="Widenet API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://widenet.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
